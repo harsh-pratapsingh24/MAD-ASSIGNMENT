11f7466685a43fce98871c5da93de7c925ab0460
harsh-pratapsingh24 <harsh.psingh2005@gmail.com>
2026-04-04 23:35:56 +0530

    Completed Q3 Sensors

--- Q3_Sensors/app/src/main/java/com/example/q3_sensors/MainActivity.java
+++ Q3_Sensors/app/src/main/java/com/example/q3_sensors/MainActivity.java
@@ -1,24 +1,87 @@
 package com.example.q3_sensors;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    // Sensor manager
+    SensorManager sensorManager;
+
+    // Sensors
+    Sensor accelerometer, lightSensor, proximitySensor;
+
+    // UI elements
+    TextView accelText, lightText, proximityText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Linking UI
+        accelText = findViewById(R.id.accelerometer);
+        lightText = findViewById(R.id.light);
+        proximityText = findViewById(R.id.proximity);
+
+        // Initialize sensor manager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Get sensors
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        // Accelerometer
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            accelText.setText("Accelerometer:\nX: " + x + "\nY: " + y + "\nZ: " + z);
+        }
+
+        // Light sensor
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
+            float value = event.values[0];
+            lightText.setText("Light: " + value);
+        }
+
+        // Proximity sensor
+        if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+            float value = event.values[0];
+            proximityText.setText("Proximity: " + value);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not required
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Register sensors
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Unregister sensors to save battery
+        sensorManager.unregisterListener(this);
     }
 }
